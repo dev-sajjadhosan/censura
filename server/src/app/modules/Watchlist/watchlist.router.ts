07bc0f7e8d74b3c8aa08e2487783b6ded2e90908
dev-sajjadhosan <devsajjadhosan@gmail.com>
2026-03-21 18:45:33 +0600

    feat: Server- watchlist module routes and middleware implement.

--- server/src/app/modules/Watchlist/watchlist.router.ts
+++ server/src/app/modules/Watchlist/watchlist.router.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { WatchlistController } from "./watchlist.controller";
 
 const router = Router();
 
+router.get("/", WatchlistController.getAllWatchlist);
+router.post("/", WatchlistController.createWatchlist);
+router.delete("/:id", WatchlistController.deleteWatchlist);
+
 export const WatchlistRouter = router;
+
+// ==> subscription comming soon,...
